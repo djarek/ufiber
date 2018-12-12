85cac4450c1b676b92d0a79da72655f79bbc1e69
Damian Jarek <damian.jarek93@gmail.com>
2018-12-12 03:54:11 +0100

    Simplify coverage collection script
    Use extraction rather than explicit removal for more reliable coverage
    collection.
    
    Signed-off-by: Damian Jarek <damian.jarek93@gmail.com>

--- tools/coverage.sh
+++ tools/coverage.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 lcov --directory build --capture --output-file build/coverage.info && \
-lcov --remove build/coverage.info '/usr/*' $(pwd)'/tests/*' $(pwd)'/build/*' --output-file build/coverage.info && \
+lcov --extract build/coverage.info $(pwd)'/include/*' --output-file build/coverage.info && \
 lcov --list build/coverage.info
