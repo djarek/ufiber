52ace18c33252ec114a48eeca6945334c69d61e2
Damian Jarek <damian.jarek93@gmail.com>
2018-12-13 01:04:07 +0100

    Add missing dependency
    Boost.Context was missing from the cmake config dependency list.
    
    Signed-off-by: Damian Jarek <damian.jarek93@gmail.com>

--- ufiberConfig.cmake
+++ ufiberConfig.cmake
@@ -1,6 +1,6 @@
 include(CMakeFindDependencyMacro)
 
-find_dependency(Boost COMPONENTS system)
+find_dependency(Boost COMPONENTS system context)
 find_dependency(Threads)
 
 include("${CMAKE_CURRENT_LIST_DIR}/ufiberTargets.cmake")
